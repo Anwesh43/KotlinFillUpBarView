4d4ef2dd809c72c85776866eeb144d3c51810007
Anwesh43 <anweshthecool0@gmail.com>
2018-03-05 02:45:41 +0530

    adding FillUpBarView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinfillupbarview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinfillupbarview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinfillupbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.fillupbarview.FillUpBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FillUpBarView.create(this)
     }
 }
