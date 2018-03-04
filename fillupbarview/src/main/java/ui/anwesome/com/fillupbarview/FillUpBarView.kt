43f63f9b67b1543dac1760204f585ddda30b5d80
Anwesh43 <anweshthecool0@gmail.com>
2018-03-05 01:39:23 +0530

    adding FillUpBarView class

--- fillupbarview/src/main/java/ui/anwesome/com/fillupbarview/FillUpBarView.kt
+++ fillupbarview/src/main/java/ui/anwesome/com/fillupbarview/FillUpBarView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.fillupbarview
 /**
  * Created by anweshmishra on 05/03/18.
  */
+import android.content.*
+import android.view.*
+import android.graphics.*
+class FillUpBarView(ctx : Context, var n : Int = 5) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
