dc3321aca86d0acb904c25972d82dc902ce02260
Anwesh43 <anweshthecool0@gmail.com>
2018-03-05 01:44:10 +0530

    adding State class

--- fillupbarview/src/main/java/ui/anwesome/com/fillupbarview/FillUpBarView.kt
+++ fillupbarview/src/main/java/ui/anwesome/com/fillupbarview/FillUpBarView.kt
@@ -19,4 +19,26 @@ class FillUpBarView(ctx : Context, var n : Int = 5) : View(ctx) {
         }
         return true
     }
+    class State(var j : Int = 0, var prevScale : Float = 0f, var dir : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += dir * 0.1f
+            if(Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j] + dir
+                j += dir
+                if(j == scales.size || j == -1) {
+                    j -= dir
+                    dir = 0
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0) {
+                dir = (1 - 2 * prevScale.toInt())
+                startcb()
+            }
+        }
+    }
 }
